8796a34ea1c89b0bb8c08a8861dda386e449f477
Justina Kuchta <justyna.kuchta@gmail.com>
2019-12-27 21:04:20 -0500

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -16,24 +16,3 @@ class Band extends Component {
 
 export default Band
 
-// import React, { Component } from 'react';
-
-// class Band extends Component {
-
-//   handleOnClick = () => {
-//     this.props.deleteBand(this.props.id)
-//   }
-
-//   render() {
-//     return(
-//       <div>
-//         <li>{this.props.name}</li>
-//         <button onClick={this.handleOnClick}>
-//           Delete
-//         </button>
-//       </div>
-//     )
-//   }
-// };
-
-// export default Band;
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -7,13 +7,13 @@ export default function manageBand(state = {
     case 'ADD_BAND':
       // debugger
       console.log(state)
-      const band = {name: action.name, id: uuid()}
-      // const band = {name: action.name, id: Math.random()*10000000000000000}
-      // return { ...state, bands: [...state.bands, band] }
-      return {bands: state.band.concat(band)}
+      // const band = {name: action.name, id: uuid()}
+      const band = {name: action.name, id: Math.random()*10000000000000000}
+      return { ...state, bands: [...state.bands, band] }
+      // return {bands: state.band.concat(band)}
     case 'DELETE_BAND': 
       console.log(state)
-      return {bands: state.bands.filter(band => band.id !== action.bandId)}
+      return {bands: state.bands.filter(band => band.id !== action.id)}
     // case 'DELETE_BAND':
     //   const bands = state.bands.filter(band => band.id !== action.bandId);
     //   return { bands };
